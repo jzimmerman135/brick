8820bb45d4b5bda4a134b63fb034218fcd17d40d
Jacob Zimmerman <jzimm@Jacobs-MBP.lan>
2022-06-18 14:12:45 -0400

    uploaded

--- brickinstall.sh
+++ brickinstall.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-mkdir ./bricktemplate/build ./bricktemplate/tests ./bricktemplate/include ./bricktemplate/src ./bricktemplate/src/mains
+# build template
+mkdir ./bricktemplate/build ./bricktemplate/tests ./bricktemplate/include
+mkdir ./bricktemplate/src ./bricktemplate/src/mains
 mv ./bricktemplate/target.c ./bricktemplate/src/mains/target.c
 
+# build stdlib linking library
+mkdir ./brickstdlib ./brickstdlib/include ./brickstdlib/build
 
-
+# move stuff deep down
+sudo cp brick /usr/local/bin
+sudo mkdir /usr/local/lib/brick
+sudo cp -r bricktemplate /usr/local/lib/brick
+sudo cp -r brickstdlib /usr/local/lib/brick
+sudo cp -r brickmessages /usr/local/lib/brick
