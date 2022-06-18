69e28c7839d1c06e7ef9cb7dc854859a59b450c7
Jacob Zimmerman <jzimm@Jacobs-MBP.lan>
2022-06-18 13:47:51 -0400

    brickinstall.sh creates template

---
+++ brickinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir ./bricktemplate/build ./bricktemplate/tests ./bricktemplate/include ./bricktemplate/src ./bricktemplate/src/mains
+mv ./bricktemplate/target.c ./bricktemplate/src/mains/target.c
+
+
+
