6bb887c133446c375389b56e135c3909caa814e1
Jacob Zimmerman <jzimm@Jacobs-MBP.lan>
2022-06-18 14:21:18 -0400

    installer build level-up:

--- brickinstall.sh
+++ brickinstall.sh
@@ -5,12 +5,17 @@ mkdir ./bricktemplate/build ./bricktemplate/tests ./bricktemplate/include
 mkdir ./bricktemplate/src ./bricktemplate/src/mains
 mv ./bricktemplate/target.c ./bricktemplate/src/mains/target.c
 
+# build .brick directory in template
+mkdir ./bricktemplate/.brick ./bricktemplate/.brick/documentation
+cp README ./bricktemplate/.brick
+cp README ./brickmessages
+
 # build stdlib linking library
 mkdir ./brickstdlib ./brickstdlib/include ./brickstdlib/build
 
 # move stuff deep down
-sudo cp brick /usr/local/bin
-sudo mkdir /usr/local/lib/brick
-sudo cp -r bricktemplate /usr/local/lib/brick
-sudo cp -r brickstdlib /usr/local/lib/brick
-sudo cp -r brickmessages /usr/local/lib/brick
+cp brick /usr/local/bin
+mkdir /usr/local/lib/brick
+cp -r bricktemplate /usr/local/lib/brick
+cp -r brickstdlib /usr/local/lib/brick
+cp -r brickmessages /usr/local/lib/brick
